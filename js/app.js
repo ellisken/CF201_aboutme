8333fbe0ab0c1d2fb7eeb10ed1aeaab9407c1192
ellisken <ellisken@oregonstate.edu>
2018-07-31 14:30:04 -0700

    Made final touches on app.js, index.html, style.css

--- js/app.js
+++ js/app.js
@@ -26,23 +26,29 @@ q5 = 'Do I prefer back end work?';
 var questions = [q1, q2, q3, q4, q5];
 var answers = ['yes', 'no', 'no', 'yes', 'yes'];
 
+
+/************************************************************
+ *                     Interaction Start
+ * ******************************************************* */
+
 //Prompt visitor for name
 var username = prompt('Hello visitor, what\'s your name?');
 console.log('visitor name entered: ' + username);
 
 //Add user welcome message to page
 document.getElementById('welcome').innerHTML += 'Welcome to my page, ' + username + '!';
+console.log('Welcome message displayed.');
 
 //Add button action
 var start_button = document.getElementById('start_game');
 start_button.onclick = function start_questions(){
   var i;
   for(i = 0; i < 5; i++){
-    console.log('Question #' + (i+1) + ": " + questions[i]);
+    console.log('Question #' + (i+1) + ': ' + questions[i]);
     var response = prompt_question(questions[i], answers[i]);
     console.log('User answer: ' + response);
   }
   return;
-}
+};
 
 
