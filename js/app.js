1e8c621713000627daf8bb4f1c10bf0ed88ead21
ellisken <ellisken@oregonstate.edu>
2018-07-31 13:17:39 -0700

    Added welcome prompt and message

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-console.log("JS is working");
+//Check JavaScript is working
+console.log('JS is working');
+
+//Prompt visitor for name
+var username = prompt('Hello visitor, what\'s your name?');
+console.log('username: ' + username);
+
+//Add user welcome message to page
+document.getElementById('welcome').innerHTML += 'Welcome to my page, ' + username + '!';
