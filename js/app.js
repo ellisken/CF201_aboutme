8741f414cd39ad5c989b9b937bf1d1809176148d
ellisken <ellisken@oregonstate.edu>
2018-08-01 13:40:33 -0700

    Added expanded answers, deleted question html section

--- js/app.js
+++ js/app.js
@@ -16,17 +16,25 @@ function prompt_question(question, answer){
   return response;
 }
 
-//Global question strings
+//Global question/answer strings
 var q1, q2, q3, q4, q5;
 q1 = 'Can I jump higher than a house?';
-q2 = 'Can I speak Korean?';
+q2 = 'Is Python my favorite computer language?';
 q3 = 'Do I have a dog?';
 q4 = 'Did I study Math in college?';
 q5 = 'Do I prefer back end work?';
 
+var a1, a2, a3, a4, a5;
+a1 = 'yes, because houses can\'t jump!';
+a2 = 'no, but it\'s fast becoming my favorite. I\'m more familiar with C.';
+a3 = 'no, but I have a Maine Coon cat named Phillip';
+a4 = 'yes, but it was not during my first degree! I love math!';
+a5 = 'yes! I am a terrible designer!';
+
+
 //Question and answer arrays
 var questions = [q1, q2, q3, q4, q5];
-var answers = ['yes', 'no', 'no', 'yes', 'yes'];
+var answers = [a1, a2, a3, a4, a5];
 
 
 /************************************************************
